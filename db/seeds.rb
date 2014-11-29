9fd4c97494d6fec04bb8288d3e898859df898de1
Andrew Mackenzie <andrew@mackenzie-serres.net>
2014-11-29 12:53:54 +0100

    Added a second solver in the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Solver.delete_all
+#...
+Solver.create!(
+  title: 'Get curl working',
+  description:
+    %{Install and configure curl on your system.}
+  )
+
+Solver.create!(
+    title: 'Get ping working',
+    description:
+        %{Install and configure ping on your system.}
+)
